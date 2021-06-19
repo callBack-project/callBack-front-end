3f294665b39687b96dfd98e73478d7dc9f677c2f
Manami Ueda <manamiueda1@gmail.com>
2021-06-19 15:32:42 -0400

    added events component using usereducer data is rendering

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Users from './components/Users/Users'
 import Jobs from './components/Jobs/Jobs'
 import Companies from './components/Companies/Companies'
+import Events from './components/Events/Events'
 
 import './App.css';
 
@@ -12,6 +13,7 @@ function App() {
       <div>
         <Switch>
           <Route exact path='/users' component={Users} />
+          <Route exact path='/events' component={Events} />
           <Route exact path='/jobs' component={Jobs} />
           <Route exact path='/companies' component={Companies} />
         </Switch>
