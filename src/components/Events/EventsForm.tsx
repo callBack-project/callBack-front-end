dfd9c7e9a260b1a5bed0e3cc9b17f8ae2eff53e4
Manami Ueda <manamiueda1@gmail.com>
2021-06-19 16:56:32 -0400

    added form

--- src/components/Events/EventsForm.tsx
+++ src/components/Events/EventsForm.tsx
@@ -1,23 +1,30 @@
+import { useState, useEffect } from "react"
+
 const EventsForm = () => {
+  const [name, setName] = useState("")
+  const [location, setLocation] = useState("")
+  const [description, setDescription] = useState("")
+  const [link, setLink] = useState("");
+  
   return (
     <div>
       <h1>Events Form</h1>
       <form>
         <label>
           Event Name:
-          <input type="text" value="name"/>
+          <input onChange={(e : any) => setName(e.target.value)} type="text" value={name}/>
         </label>
         <label>
           Location:
-          <input type="text" value="location"/>
+          <input onChange={(e: any) => setLocation(e.target.value)} type="text" value={location}/>
         </label>
         <label>
           Description:
-          <input type="text" value="description"/>
+          <input onChange={(e: any) => setDescription(e.target.value)} type="text" value={description}/>
         </label>
         <label>
           Link:
-          <input type="text" value="link"/>
+          <input onChange={(e: any) => setLink(e.target.value)} type="text" value={link}/>
         </label>
         <input type="submit" value="Submit"/>
       </form>
