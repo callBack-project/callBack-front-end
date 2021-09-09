e1f5696686e2d7499738d46e8c690cce761f63ae
Alex Paul <al.9thousand@gmail.com>
2021-09-08 21:17:22 -0400

    add logout functionality

--- src/components/Home/HomeComponent.tsx
+++ src/components/Home/HomeComponent.tsx
@@ -1,14 +1,32 @@
-import { Redirect } from "react-router-dom";
+import { Redirect, useHistory } from "react-router-dom";
 import { useContext } from "react"
-// import { loginReducer, ACTIONS, initialState } from '../Login/LoginReducer';
+import { ACTIONS, initialState } from '../Login/LoginReducer';
 import { AppContext } from '../Context/context'
+
+import axios from 'axios'
+//import { initialState } from "../Users/UsersReducer";
+
+const instance = axios.create({
+  withCredentials: true
+})
 
 const HomeComponent = () => {
   //const { user, handleClick } = props;
   //const [state, dispatch] = useReducer(loginReducer, initialState);
   const { state, dispatch } = useContext(AppContext)
   const { user, loading, error } = state;
-  console.log("home comp user->", state)
+  const history = useHistory()
+  
+  const logoutSubmitHandler =  async () => {
+    try {
+      const response = await instance.delete('http://localhost:8080/api/auth/logout');
+      await dispatch({ type: ACTIONS.SUCCESS, data: initialState.user });
+      await history.push('/')
+    } catch (error) {
+      dispatch({ type: ACTIONS.ERROR, error: error.message || error });
+    }
+  }
+
   if (!user.id) {
     return <Redirect to="/" />;
   }
@@ -20,7 +38,7 @@ const HomeComponent = () => {
         <h1>Welcome back {user.firstName}!</h1>
       </div>
       <div>
-        <button className="btn bg-red white p1 rounded" /*onClick={handleClick}*/>
+        <button className="btn bg-red white p1 rounded" onClick={() => logoutSubmitHandler()}>
           Logout
         </button>
       </div>
